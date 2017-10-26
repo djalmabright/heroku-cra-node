d13e2ac39bc90fed45edcffbd814c2c10ddccb01
Djalma  Bina <djbina2000@yahoo.com.br>
2017-10-26 18:20:02 -0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 app.get('/api', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.send('{"message":"Hello from my custom server!"}');
-res.write('<!DOCTYPE html><html lang="en"><head>');  
+res.write('');  
 res.end();  
 });
 
